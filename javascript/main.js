1c87f49d58d8d5c1a3fcdde393215a568893f335
Aditi024 <aditi024@bu.edu>
2018-10-18 00:32:40 -0400

    Sketch
    Sketch

---
+++ javascript/main.js
@@ -0,0 +1,45 @@
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d'),
+    W = canvas.width = "1000",
+    H = canvas.height = "500",
+    mouseX,
+    mouseY,
+    lastMouseX,
+    lastMouseY,
+    angle = 0,
+    width = 15,
+    range = 80,
+    speed = 0.08;
+
+
+canvas.addEventListener('mousemove', function(e) {
+  mouseX = e.pageX - this.offsetLeft;
+  mouseY = e.pageY - this.offsetTop;
+});
+
+window.addEventListener('keydown', function() {
+  ctx.clearRect(0, 0, W, H);
+});
+
+
+;(function draw() {
+  requestAnimationFrame(draw);
+  //ctx.clearRect(0, 0, W, H);
+
+  var size = width + Math.sin(angle); /* range */
+  ctx.lineWidth = size;
+  angle += speed;
+
+  ctx.beginPath();
+  ctx.moveTo(lastMouseX, lastMouseY);
+  ctx.lineTo(mouseX, mouseY);
+  ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
+  ctx.strokeStyle = '#373AFF';
+  ctx.stroke();
+
+
+  lastMouseX = mouseX;
+  lastMouseY = mouseY;
+
+})();
